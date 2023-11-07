3a5fbb06c02ab76d0e6815bb98e352dc1ed19c1b
paragonnov <qkdxorjs1002@naver.com>
2023-11-07 11:08:53 +0900

    테스트 코드 정리

--- src/test/java/KkmaWrapperTest.java
+++ src/test/java/KkmaWrapperTest.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import kr.paragonnov.KkmaWrapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -12,77 +13,94 @@ public class KkmaWrapperTest {
     @DisplayName("Command-line Test (--help)")
     @Test
     public void commandTestHelp() {
+        final String expectedResult = KkmaWrapper.helpMessage;
         final String[] args = {
                 "--help",
                 "--disable-print-output",
                 "안녕하세요, 반가워요. 잘 있어요. 다시 만나요."
         };
 
-        final String result = KkmaWrapper.runKkma(args);
-        System.out.println(result);
+        final String actualResult = KkmaWrapper.runKkma(args);
+        System.out.println(actualResult);
 
         Assertions.assertEquals(
-                KkmaWrapper.helpMessage,
-                result
+                expectedResult,
+                actualResult
         );
     }
 
     @DisplayName("Command-line Test")
     @Test
     public void commandTest() {
+        final String expectedResult = "[[\"안녕/NNG\",\"하세/NNG\",\"요/JX\",\",/SP\",\"반갑/VA\",\"어요/EFN\",\"./SF\",\"잘/MAG\",\"있/VA\",\"어요/EFN\",\"./SF\",\"다시/MAG\",\"만나/VV\",\"아요/EFN\",\"./SF\"]]";
         final String[] args = {
                 "--log",
                 "--disable-print-output",
                 "안녕하세요, 반가워요. 잘 있어요. 다시 만나요."
         };
 
-        final String result = KkmaWrapper.runKkma(args);
-        System.out.println(result);
+        final String actualResult = KkmaWrapper.runKkma(args);
+        System.out.println(actualResult);
 
         Assertions.assertEquals(
-                "[[\"안녕/NNG\",\"하세/NNG\",\"요/JX\",\",/SP\",\"반갑/VA\",\"어요/EFN\",\"./SF\",\"잘/MAG\",\"있/VA\",\"어요/EFN\",\"./SF\",\"다시/MAG\",\"만나/VV\",\"아요/EFN\",\"./SF\"]]",
-                result
+                expectedResult,
+                actualResult
         );
     }
 
     @DisplayName("Command-line Test (List)")
     @Test
     public void commandTestList() {
-        final String[] args = {
-                "--log",
-                "--disable-print-output",
-                "0안녕하세요, 반가워요. 잘 있어요. 다시 만나요.",
-                "1안녕하세요, 반가워요. 잘 있어요. 다시 만나요.",
-                "2안녕하세요, 반가워요. 잘 있어요. 다시 만나요.",
-                "3안녕하세요, 반가워요. 잘 있어요. 다시 만나요.",
-                "4안녕하세요, 반가워요. 잘 있어요. 다시 만나요."
-        };
+        final List<List<String>> expectedMorphList = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            expectedMorphList.add(List.of(Integer.toString(i).concat("/NR"), "안녕/NNG", "하세/NNG", "요/JX", ",/SP", "반갑/VA", "어요/EFN", "./SF", "잘/MAG", "있/VA", "어요/EFN", "./SF", "다시/MAG", "만나/VV", "아요/EFN", "./SF"));
+        }
+        final String expectedResult = new Gson().toJson(expectedMorphList);
 
-        final String result = KkmaWrapper.runKkma(args);
-        System.out.println(result);
+        final List<String> argList = new ArrayList<>(List.of("--log", "--disable-print-output"));
+        for (int i = 0; i < 4; i++) {
+            argList.add(Integer.toString(i).concat("안녕하세요, 반가워요. 잘 있어요. 다시 만나요."));
+        }
+        final String[] args = argList.toArray(String[]::new);
+
+        final String actualResult = KkmaWrapper.runKkma(args);
+        System.out.println(actualResult);
 
         Assertions.assertEquals(
-                "[[\"0/NR\",\"안녕/NNG\",\"하세/NNG\",\"요/JX\",\",/SP\",\"반갑/VA\",\"어요/EFN\",\"./SF\",\"잘/MAG\",\"있/VA\",\"어요/EFN\",\"./SF\",\"다시/MAG\",\"만나/VV\",\"아요/EFN\",\"./SF\"],[\"1/NR\",\"안녕/NNG\",\"하세/NNG\",\"요/JX\",\",/SP\",\"반갑/VA\",\"어요/EFN\",\"./SF\",\"잘/MAG\",\"있/VA\",\"어요/EFN\",\"./SF\",\"다시/MAG\",\"만나/VV\",\"아요/EFN\",\"./SF\"],[\"2/NR\",\"안녕/NNG\",\"하세/NNG\",\"요/JX\",\",/SP\",\"반갑/VA\",\"어요/EFN\",\"./SF\",\"잘/MAG\",\"있/VA\",\"어요/EFN\",\"./SF\",\"다시/MAG\",\"만나/VV\",\"아요/EFN\",\"./SF\"],[\"3/NR\",\"안녕/NNG\",\"하세/NNG\",\"요/JX\",\",/SP\",\"반갑/VA\",\"어요/EFN\",\"./SF\",\"잘/MAG\",\"있/VA\",\"어요/EFN\",\"./SF\",\"다시/MAG\",\"만나/VV\",\"아요/EFN\",\"./SF\"],[\"4/NR\",\"안녕/NNG\",\"하세/NNG\",\"요/JX\",\",/SP\",\"반갑/VA\",\"어요/EFN\",\"./SF\",\"잘/MAG\",\"있/VA\",\"어요/EFN\",\"./SF\",\"다시/MAG\",\"만나/VV\",\"아요/EFN\",\"./SF\"]]",
-                result
+                expectedResult,
+                actualResult
         );
     }
 
     @DisplayName("Command-line Test (Mass List)")
     @Test
     public void commandTestMassList() {
+        final List<List<String>> expectedMorphList = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            expectedMorphList.add(List.of("안녕/NNG", "하세/NNG", "요/JX", ",/SP", "반갑/VA", "어요/EFN", "./SF", "잘/MAG", "있/VA", "어요/EFN", "./SF", "다시/MAG", "만나/VV", "아요/EFN", "./SF", Integer.toString(i).concat("/NR")));
+        }
+        final String expectedResult = new Gson().toJson(expectedMorphList);
+
         final List<String> argList = new ArrayList<>(List.of("--log", "--disable-print-output"));
         for (int i = 0; i < 1000; i++) {
             argList.add("안녕하세요, 반가워요. 잘 있어요. 다시 만나요.".concat(Integer.toString(i)));
         }
         final String[] args = argList.toArray(String[]::new);
 
-        final String result = KkmaWrapper.runKkma(args);
-        System.out.println(result);
+        final String actualResult = KkmaWrapper.runKkma(args);
+        System.out.println(actualResult);
+
+        Assertions.assertEquals(
+                expectedResult,
+                actualResult
+        );
     }
 
     @DisplayName("Command-line Test (--b64)")
     @Test
     public void commandTestB64() {
+        final String expectedResult = "[[\"안녕/NNG\",\"하세/NNG\",\"요/JX\",\",/SP\",\"반갑/VA\",\"어요/EFN\",\"./SF\",\"잘/MAG\",\"있/VA\",\"어요/EFN\",\"./SF\",\"다시/MAG\",\"만나/VV\",\"아요/EFN\",\"./SF\"]]";
+
         final String[] args = {
                 "--log",
                 "--b64",
@@ -90,18 +108,25 @@ public class KkmaWrapperTest {
                 Base64.getEncoder().encodeToString("안녕하세요, 반가워요. 잘 있어요. 다시 만나요.".getBytes())
         };
 
-        final String result = KkmaWrapper.runKkma(args);
-        System.out.println(new String(Base64.getDecoder().decode(result)));
+        final String actualResult = KkmaWrapper.runKkma(args);
+        final String decodedActualResult = new String(Base64.getDecoder().decode(actualResult));
+        System.out.println(decodedActualResult);
 
         Assertions.assertEquals(
-                "W1si7JWI64WVL05ORyIsIu2VmOyEuC9OTkciLCLsmpQvSlgiLCIsL1NQIiwi67CY6rCRL1ZBIiwi7Ja07JqUL0VGTiIsIi4vU0YiLCLsnpgvTUFHIiwi7J6IL1ZBIiwi7Ja07JqUL0VGTiIsIi4vU0YiLCLri6Tsi5wvTUFHIiwi66eM64KYL1ZWIiwi7JWE7JqUL0VGTiIsIi4vU0YiXV0=",
-                result
+                expectedResult,
+                decodedActualResult
         );
     }
 
     @DisplayName("Command-line Test (--b64 & List)")
     @Test
     public void commandTestB64List() {
+        final List<List<String>> expectedMorphList = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            expectedMorphList.add(List.of(Integer.toString(i).concat("/NR"), "안녕/NNG", "하세/NNG", "요/JX", ",/SP", "반갑/VA", "어요/EFN", "./SF", "잘/MAG", "있/VA", "어요/EFN", "./SF", "다시/MAG", "만나/VV", "아요/EFN", "./SF"));
+        }
+        final String expectedResult = new Gson().toJson(expectedMorphList);
+
         final String[] args = {
                 "--log",
                 "--b64",
@@ -112,12 +137,13 @@ public class KkmaWrapperTest {
                 Base64.getEncoder().encodeToString("3안녕하세요, 반가워요. 잘 있어요. 다시 만나요.".getBytes())
         };
 
-        final String result = KkmaWrapper.runKkma(args);
-        System.out.println(new String(Base64.getDecoder().decode(result)));
+        final String actualResult = KkmaWrapper.runKkma(args);
+        final String decodedActualResult = new String(Base64.getDecoder().decode(actualResult));
+        System.out.println(decodedActualResult);
 
         Assertions.assertEquals(
-                "W1siMC9OUiIsIuyViOuFlS9OTkciLCLtlZjshLgvTk5HIiwi7JqUL0pYIiwiLC9TUCIsIuuwmOqwkS9WQSIsIuyWtOyalC9FRk4iLCIuL1NGIiwi7J6YL01BRyIsIuyeiC9WQSIsIuyWtOyalC9FRk4iLCIuL1NGIiwi64uk7IucL01BRyIsIuunjOuCmC9WViIsIuyVhOyalC9FRk4iLCIuL1NGIl0sWyIxL05SIiwi7JWI64WVL05ORyIsIu2VmOyEuC9OTkciLCLsmpQvSlgiLCIsL1NQIiwi67CY6rCRL1ZBIiwi7Ja07JqUL0VGTiIsIi4vU0YiLCLsnpgvTUFHIiwi7J6IL1ZBIiwi7Ja07JqUL0VGTiIsIi4vU0YiLCLri6Tsi5wvTUFHIiwi66eM64KYL1ZWIiwi7JWE7JqUL0VGTiIsIi4vU0YiXSxbIjIvTlIiLCLslYjrhZUvTk5HIiwi7ZWY7IS4L05ORyIsIuyalC9KWCIsIiwvU1AiLCLrsJjqsJEvVkEiLCLslrTsmpQvRUZOIiwiLi9TRiIsIuyemC9NQUciLCLsnogvVkEiLCLslrTsmpQvRUZOIiwiLi9TRiIsIuuLpOyLnC9NQUciLCLrp4zrgpgvVlYiLCLslYTsmpQvRUZOIiwiLi9TRiJdLFsiMy9OUiIsIuyViOuFlS9OTkciLCLtlZjshLgvTk5HIiwi7JqUL0pYIiwiLC9TUCIsIuuwmOqwkS9WQSIsIuyWtOyalC9FRk4iLCIuL1NGIiwi7J6YL01BRyIsIuyeiC9WQSIsIuyWtOyalC9FRk4iLCIuL1NGIiwi64uk7IucL01BRyIsIuunjOuCmC9WViIsIuyVhOyalC9FRk4iLCIuL1NGIl1d",
-                result
+                expectedResult,
+                decodedActualResult
         );
     }
 }
